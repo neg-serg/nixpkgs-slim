f04cb36c886f11d9a57149b1baea66e8ac4f0a43
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-14 21:49:23 +0300

    Fix: uncomment rsyslogd

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -756,7 +756,7 @@
   #./services/logging/logcheck.nix
   ./services/logging/logrotate.nix
   #./services/logging/logstash.nix
-  #./services/logging/rsyslogd.nix
+  ./services/logging/rsyslogd.nix
   #./services/logging/syslog-ng.nix
   #./services/logging/syslogd.nix
   #./services/logging/SystemdJournal2Gelf.nix
